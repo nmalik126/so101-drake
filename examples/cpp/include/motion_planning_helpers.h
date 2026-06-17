e85eb2ea02dcf97dfd8f5ce7f2647e0f8301cddc
nmalik126 <noorehmadmalik@gmail.com>
2026-06-17 14:21:57 -0600

    hardware demo refactor

--- examples/cpp/include/motion_planning_helpers.h
+++ examples/cpp/include/motion_planning_helpers.h
@@ -12,6 +12,8 @@
 #include <drake/common/trajectories/piecewise_polynomial.h>
 #include <drake/common/copyable_unique_ptr.h>
 #include <drake/planning/scene_graph_collision_checker.h>
+#include <drake/planning/collision_checker_params.h>
+#include <drake/math/rigid_transform.h>
 
 #include <Eigen/Dense>
 
@@ -23,6 +25,8 @@ using motion_planning::ompl::GenerateWaypoints;
 using motion_planning::trajectory_optimization::GenerateTrajectory;
 using motion_planning::inverse_kinematics::GeneratePlaceConfig;
 using motion_planning::inverse_kinematics::SO101InverseKinematics;
+using motion_planning::inverse_kinematics::SO101InverseKinematicsPick;
+using motion_planning::inverse_kinematics::SO101InverseKinematicsPlace;
 using motion_planning::ompl::SO101OMPL;
 using motion_planning::trajectory_optimization::SO101TrajOpt;
 
@@ -33,6 +37,8 @@ using drake::geometry::MeshcatVisualizer;
 using drake::trajectories::CompositeTrajectory;
 using drake::trajectories::PiecewisePolynomial;
 using drake::planning::SceneGraphCollisionChecker;
+using drake::planning::CollisionCheckerParams;
+using drake::math::RigidTransformd;
 
 namespace motion_planning {
 
@@ -300,6 +306,126 @@ inline std::optional<BsplineTrajectory<double>> GenerateMotionPlan(
     return detail::GenerateMotionPlanImpl(
         sampling_planner, trajopt, q_start, q_goal, checker
     );
+}
+
+inline std::optional<CompositeTrajectory<double>> GeneratePickPlaceMotionPlans() {
+    // create plan scenario
+    std::cout << "creating plan scenario..." << std::endl;
+    auto plan_assets { helpers::generate_so101_brick_diagram(true, false) };
+    auto& plan_plant { *(plan_assets.plant) };
+    auto plan_diagram { plan_assets.diagram };
+    std::cout << "plan scenario created." << std::endl;
+
+    // create collision checker
+    std::cout << "creating collision checker..." << std::endl;
+    auto checker { std::make_shared<SceneGraphCollisionChecker>(
+        CollisionCheckerParams {
+            .model { plan_diagram },
+            .robot_model_instances { {
+                plan_plant.GetModelInstanceByName("so101_new_calib")
+            } },
+            .edge_step_size { 0.01 }
+        }
+    ) };
+    std::cout << "collision checker created." << std::endl;
+
+    // create motion planning objects
+    SO101InverseKinematicsPick ik_pick { plan_diagram };
+    SO101InverseKinematicsPlace ik_place { plan_diagram };
+    SO101OMPL sampling_planner { plan_diagram, checker };
+    SO101TrajOpt trajopt { plan_diagram, checker };
+    const Eigen::VectorXd q_init { 
+        Eigen::VectorXd::Map(
+            constants::SO101_Q_INIT, constants::SO101_NUM_Q
+        ) 
+    };
+
+    // solve pick plan
+    std::cout << "solving pick plan..." << std::endl;
+    auto pick_result { motion_planning::GenerateMotionPlan(
+        ik_pick, sampling_planner, trajopt, q_init, checker
+    ) };
+    if (!pick_result) {
+        std::cout << "Pick Plan Failed. Exiting..." << std::endl;
+        return std::nullopt;
+    }
+    const auto pick_trajectory { pick_result.value() };
+
+    // compute gripper close trajectory
+    const Eigen::VectorXd q_pick_open { pick_trajectory.FinalValue() };
+    Eigen::VectorXd q_pick_closed { q_pick_open };
+    q_pick_closed(constants::SO101_NUM_Q - 1) = -0.1;
+    const auto gripper_close_trajectory {
+        PiecewisePolynomial<double>::FirstOrderHold(
+            { 0.0, 1.0 },
+            { q_pick_open, q_pick_closed }
+        )
+    };
+
+    // filter collisions between welded box and gripper bodies
+    const auto& box_body { plan_plant.GetBodyByName("box_link") };
+    const auto& gripper_link_body { plan_plant.GetBodyByName("gripper_link") };
+    const auto& moving_jaw_body { plan_plant.GetBodyByName("moving_jaw_so101_v1_link") };
+    checker->SetCollisionFilteredBetween(box_body.index(), gripper_link_body.index(), true);
+    checker->SetCollisionFilteredBetween(box_body.index(), moving_jaw_body.index(), true);
+
+    // add box collision geometry to gripper body
+    auto fk_context { plan_plant.CreateDefaultContext() };
+    plan_plant.SetPositions(fk_context.get(), q_pick_closed);
+    const RigidTransformd X_world_gripper { plan_plant.EvalBodyPoseInWorld(*fk_context, gripper_link_body) };
+    const RigidTransformd X_world_box { plan_plant.EvalBodyPoseInWorld(*fk_context, box_body) };
+    const RigidTransformd X_gripper_box { X_world_gripper.inverse() * X_world_box };
+    checker->AddCollisionShapeToBody(
+        "grasped_box",
+        gripper_link_body,
+        drake::geometry::Box(0.04, 0.03, 0.03),
+        X_gripper_box
+    );
+    
+    // solve place plan
+    std::cout << "solving place plan..." << std::endl;
+    auto place_result { motion_planning::GenerateMotionPlan(
+        ik_place, sampling_planner, trajopt, q_pick_closed, checker
+    ) };
+    if (!place_result) {
+        std::cout << "Place Plan Failed. Exiting..." << std::endl;
+        return std::nullopt;
+    }
+    const auto place_trajectory { place_result.value() };
+
+    // compute gripper open trajectory
+    const Eigen::VectorXd q_place_closed { place_trajectory.FinalValue() };
+    Eigen::VectorXd q_place_open { q_place_closed };
+    q_place_open(constants::SO101_NUM_Q - 1) = 0.5;
+    const auto gripper_open_trajectory {
+        PiecewisePolynomial<double>::FirstOrderHold(
+            { 0.0, 1.0 },
+            { q_place_closed, q_place_open }
+        )
+    };
+
+    // remove box collision geometry from gripper body
+    checker->RemoveAllAddedCollisionShapes("grasped_box");
+
+    // solve rest plan
+    std::cout << "solving rest plan..." << std::endl;
+    auto rest_result { motion_planning::GenerateMotionPlan(
+        sampling_planner, trajopt, q_place_open, q_init, checker
+    ) };
+    if (!rest_result) {
+        std::cout << "Rest Plan Failed. Exiting..." << std::endl;
+        return std::nullopt;
+    }
+    const auto rest_trajectory { rest_result.value() };
+    
+    // construct composite trajectory
+    return CompositeTrajectory<double>::AlignAndConcatenate({
+        drake::copyable_unique_ptr<Trajectory<double>> { pick_trajectory },
+        drake::copyable_unique_ptr<Trajectory<double>> { gripper_close_trajectory },
+        drake::copyable_unique_ptr<Trajectory<double>> { place_trajectory },
+        drake::copyable_unique_ptr<Trajectory<double>> { gripper_open_trajectory },
+        drake::copyable_unique_ptr<Trajectory<double>> { rest_trajectory },
+    });
 }
 
 } // namespace motion_planning
---
+++ examples/cpp/src/hardware_refactor.cpp
@@ -0,0 +1,142 @@
+#include "hardware/so101_lcm.h"
+#include "helpers.h"
+#include "constants.h"
+#include "motion_planning_helpers.h"
+
+#include "messages/so101/lcmt_so101_configuration.hpp"
+
+#include <drake/systems/framework/diagram_builder.h>
+#include <drake/geometry/scene_graph.h>
+#include <drake/multibody/plant/multibody_plant.h>
+#include <drake/multibody/parsing/parser.h>
+#include <drake/geometry/meshcat.h>
+#include <drake/geometry/meshcat_visualizer.h>
+#include <drake/systems/analysis/simulator.h>
+#include <drake/systems/lcm/lcm_interface_system.h>
+#include <drake/systems/lcm/lcm_subscriber_system.h>
+#include <drake/systems/lcm/lcm_publisher_system.h>
+#include <drake/systems/rendering/multibody_position_to_geometry_pose.h>
+#include <drake/systems/primitives/trajectory_source.h>
+
+#include <Eigen/Dense>
+
+#include <iostream>
+
+using motion_planning::hardware::SO101StatusReceiver;
+using motion_planning::hardware::SO101CommandSender;
+using motion_planning::GeneratePickPlaceMotionPlans;
+
+using drake::systems::DiagramBuilder;
+using drake::geometry::SceneGraph;
+using drake::multibody::MultibodyPlant;
+using drake::multibody::Parser;
+using drake::geometry::Meshcat;
+using drake::geometry::MeshcatVisualizer;
+using drake::systems::Simulator;
+using drake::systems::lcm::LcmInterfaceSystem;
+using drake::systems::lcm::LcmSubscriberSystem;
+using drake::systems::lcm::LcmPublisherSystem;
+using drake::systems::rendering::MultibodyPositionToGeometryPose;
+using drake::systems::TrajectorySource;
+
+int main() {
+    std::cout << "hardware refactor" << '\n';
+
+    // init diagram
+    DiagramBuilder<double> diagram_builder {};
+    auto* scene_graph {
+        diagram_builder.AddSystem<SceneGraph>()
+    };
+    MultibodyPlant<double> plant { 1e-3 };
+    plant.RegisterAsSourceForSceneGraph(scene_graph);
+    Parser parser { &plant };
+
+    // init scenario
+    std::cout << "parsing started..." << '\n';
+    helpers::generate_so101_brick_welded(plant, *scene_graph, parser);
+    std::cout << "parsing finished." << '\n';
+
+    // init meshcat
+    auto meshcat { std::make_shared<Meshcat>() };
+    auto& visualizer { MeshcatVisualizer<double>::AddToBuilder(
+        &diagram_builder, *scene_graph, meshcat
+    ) };
+
+    // init lcm
+    auto* lcm {
+        diagram_builder.AddSystem<LcmInterfaceSystem>()
+    };
+
+    // create status subscriber and receiver
+    auto* status_sub { diagram_builder.AddSystem(
+        LcmSubscriberSystem::Make<so101::lcmt_so101_configuration>(
+            "SO101_STATUS", lcm
+        )
+    ) };
+    auto* status_receiver {
+        diagram_builder.AddSystem<SO101StatusReceiver>()
+    };
+    diagram_builder.Connect(
+        status_sub->get_output_port(),
+        status_receiver->get_input_port(0)
+    );
+
+    // create command publisher and sender
+    auto* command_pub { diagram_builder.AddSystem(
+        LcmPublisherSystem::Make<so101::lcmt_so101_configuration>(
+            "SO101_COMMAND", lcm, 0.005
+        )
+    ) };
+    auto* command_sender {
+        diagram_builder.AddSystem<SO101CommandSender>()
+    };
+    diagram_builder.Connect(
+        command_sender->get_output_port(0),
+        command_pub->get_input_port()
+    );
+
+    // wire to-pose
+    auto* to_pose {
+        diagram_builder.AddSystem<MultibodyPositionToGeometryPose>(plant)
+    };
+    diagram_builder.Connect(
+        to_pose->get_output_port(),
+        scene_graph->get_source_pose_port(
+            plant.get_source_id().value()
+        )
+    );
+    diagram_builder.Connect(
+        status_receiver->get_output_port(),
+        to_pose->get_input_port()
+    );
+
+    // wire trajectory source
+    const auto pick_place_result { GeneratePickPlaceMotionPlans() };
+    if (!pick_place_result) {
+        std::cout << "Pick and Place Computation Failed. Exiting..." << '\n';
+        return 1;
+    }
+    const auto trajectory { pick_place_result.value() };
+    auto q_source {
+        diagram_builder.AddSystem<TrajectorySource>(trajectory)
+    };
+    diagram_builder.Connect(
+        q_source->get_output_port(),
+        command_sender->get_input_port()
+    );
+
+    // build diagram
+    auto diagram { diagram_builder.Build() };
+    
+    // simulate
+    std::cout << "simulating..." << '\n';
+    Simulator<double> simulator { *diagram };
+    simulator.set_target_realtime_rate(1.0);
+    meshcat->StartRecording();
+    simulator.AdvanceTo(trajectory.end_time());
+    meshcat->StopRecording();
+    meshcat->PublishRecording();
+    helpers::user_input_quit();
+
+    return 0;
+}
